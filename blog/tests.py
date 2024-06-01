0a55771686f92e775f082bf2606a58a2e511f909
Ubuntu <ubuntu@ip-172-31-11-21.eu-central-1.compute.internal>
2024-06-01 09:36:09 +0000

    updated actions file

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(title="Test1 title", author="Author test", slug="slug test")
+        
+    def test_post_model(self):
+        blog = self.blog
+        self.assertTrue(isinstance(blog, Post))
+    
+    def test_post_return(self):
+        blog = self.blog
+        self.assertEqual(str(blog), 'Test1 title')
+        
